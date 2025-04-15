d7ed39254f02debb55d4666b151b8fcbbbd7c0dd
YahirZapata <yahirrrrafk@gmail.com>
2025-04-15 19:00:33 -0300

    desarrollamos el frontend

--- index.php
+++ index.php
@@ -4,20 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/chatbot/diseño/diseñoIndex.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Inicio</title>
     <!---->
 </head>
 <body>
     <header>
         <h1>Chatbot - DADA DADA</h1>
-            <nav>
-                AltaConsulta
-                EditarConsulta
-                Chatbot
-            </nav>
     </header>
 
     <main>
+    <div>
+        <nav>
+            <span class="texto1">Consulta</span>
+            <span class="texto2">Editar Consulta</span>
+            <span class="texto3">Chatbot</span>
+        </nav>  
+    </div>
         <section class="descripcionPJ">
             <!--Descripción del personaje-->
         </section>
@@ -29,7 +34,7 @@
     <footer>
         <p>Logo del colegio</p>
         <p>Integrantes</p>
-        <p>Nro del grupo</p>
+        <p>Nro del grupo</p>        
         <p>Curso y division</p>
         <p>Materia</p>
     </footer>
