1a986ac6bc527620203e942bf38a3392008dde71
YahirZapata <yahirrrrafk@gmail.com>
2025-04-29 19:06:20 -0300

    añadimos a gaston

--- index.php
+++ index.php
@@ -7,12 +7,35 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <title>Inicio</title>
     <!---->
 </head>
 <body>
     <header>
-        <h1>Chatbot - DADA DADA</h1>
+        <h1>
+            Chatbot - Gastón
+        </h1>
+        <div class="wrapper">
+    <div class="title">Chatea con Gastón</div>
+    <div class="form">
+        <div class="bot-inbox inbox">
+            <div class="icon">
+                <i class="fas fa-user"></i>
+            </div>
+            <div class="msg-header">
+                <p>Hola, soy Gastón, ¿cómo puedo ayudarte?</p>
+            </div>
+        </div>
+    </div>
+    <div class="typing-field">
+        <div class="input-data">
+            <input id="data" type="text" placeholder="Escribe algo aquí.." required>
+            <button id="send-btn">Enviar</button>
+        </div>
+    </div>
+</div>
     </header>
 
     <main>
@@ -32,12 +55,21 @@
     </main>
 
     <footer>
+         <!--
         <p>Logo del colegio</p>
         <p>Integrantes</p>
         <p>Nro del grupo</p>        
         <p>Curso y division</p>
         <p>Materia</p>
+        -->
     </footer>
 
+
 </body>
+ <!--<script>
+    $(document).ready(function{}{
+        $("#send-btn").on"click"(function(){
+
+    });
+</script>-->
 </html>
