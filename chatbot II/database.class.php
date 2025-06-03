81f9508d1a8e001625f7de7037d65fe34ebd4d7d
YahirZapata <yahirrrrafk@gmail.com>
2025-06-03 19:03:36 -0300

    instancia

--- chatbot II/database.class.php
+++ chatbot II/database.class.php
@@ -7,16 +7,30 @@ class Database {
     private $clave= "";
     private $conexion;
 
+    private $instance = null;
+
     public function __construct() {
+        $dnfs = "mysql:host=$this->servidor;dbname=$this->nombre;charset=utf8mb4";
+        try {
+            $this->conexion = new PDO($dnfs, $this->usuario, $this->clave);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } die (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+        }
 
     }
 
     public function getConnection(){
+        if(!self::instancia){
+            self::instancia = new Database();
+        }
+        return self::instancia;
 
     }
 
     public function execute (){
-
+        
     }
 
     public function query(){
--- chatbot II/rol.class.php
+++ chatbot II/rol.class.php
@@ -1,19 +1,31 @@
 <?php
-class Roles {
+include_once 'database.class.php';
+// usamos el metodo sin crear el objeto
+$roles = Rol::ObtenerTodas();
+
+class Rol {
     private $id;
     private $nombre;
     private $conexion;
 
-    public function __construct($conexion) {
-        // ...
+    public function __construct($id=null, $nombre=null) {
+        $this->id=$id;
+        $this->nombre=$nombre;
+        $this->conexion = Database::getInstance()->getConnection();
     }
 
     public function guardar() {
-        // ...
+        $sql = "INSERT INTO roles (nombre) VALUES (?)";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$this->nombre]);
     }
 
     public function obtenerTodas() {
-        // ...
+        $conexion = Database::getInstance()->getConnection();
+        $sql = "SELECT * FROM roles";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function obtenerPorId($id) {
@@ -21,10 +33,35 @@ class Roles {
     }
 
     public function actualizar($id, $nombre) {
-        // ...
+        $sql = "UPDATE roles SET nombre= ? WHERE id = ?";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$this->nombre, $this->id]);
+    }
+
+
     }
 
     public function eliminar($id) {
-        // ...
+        $sql = "DELETE FROM roles WHERE id = ?";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$id]);
     }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
 }
+
+
+
+?>
