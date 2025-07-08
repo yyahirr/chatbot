e39f8b792c391bc07b4afd93290a29a360ecfda4
YahirZapata <yahirrrrafk@gmail.com>
2025-07-08 19:04:43 -0300

    falta hacer el editar de respuesta

--- chatbot2/model/pregunta.class.php
+++ chatbot2/model/pregunta.class.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'Database.class.php';
+include_once 'database.class.php';
 
 class Preguntas {
     private $id;
--- chatbot2/model/respuesta.class.php
+++ chatbot2/model/respuesta.class.php
@@ -1,31 +1,77 @@
 <?php
+include_once 'database.class.php';
+
 class Respuesta {
     private $id;
     private $respuesta;
     private $pregunta_id;
     private $conexion;
 
-    public function __construct($conexion) {
-        // ...
+    public function __construct($id = null, $respuesta = null, $pregunta_id = null) {
+        $this->id = $id;
+        $this->respuesta = $respuesta;
+        $this->pregunta_id = $pregunta_id;
+        $this->conexion = Database::GetConnection();
     }
 
+
+    public static function obtenerTodas() {
+        $conexion = Database::GetConnection();
+        $sql = "SELECT * from respuesta";
+        $stmt = $conexion->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function obtenerPorId($id) {
+        $conexion = Database::GetConnection();
+        $sql = "SELECT * from respuesta WHERE id = ?";
+        $stmt = $conexion->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
     public function guardar() {
-        // ...
+        $sql = "INSERT INTO respuesta (respuesta, pregunta_id) VALUES (?, ?)";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$this->respuesta, $this->pregunta_id]);
     }
 
-    public function obtenerTodas() {
-        // ...
-    }
-
-    public function obtenerPorId($id) {
-        // ...
-    }
-
-    public function actualizar($id, $respuesta, $pregunta_id) {
-        // ...
+    public function actualizar() {
+        $sql = "UPDATE respuesta SET respuesta = ?, pregunta_id = ? WHERE id = ?";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$this->respuesta, $this->pregunta_id, $this->id]);
     }
 
     public function eliminar($id) {
-        // ...
+        $sql = "DELETE FROM respuesta WHERE id = ?";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getRespuesta() {
+        return $this->respuesta;
+    }
+
+    public function getPreguntaId() {
+        return $this->pregunta_id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setPreguntaId($pregunta_id) {
+        $this->pregunta_id = $pregunta_id;
+    }
+
+    public function setRespuesta($respuesta) {
+        $this->respuesta = $respuesta;
     }
 }
+
+
+
